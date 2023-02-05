0ee4adac0720def2189a92b7e61a84e36db174c6
berteek <merzinalbert@gmail.com>
2023-02-05 16:36:45 +0400

    feat: add brain-gcd game

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,23 @@
+import brain_games.game as game
+import math
+from random import randint
+
+
+def get_question() -> str:
+    x = randint(0, 30)
+    y = randint(0, 30)
+    question = f'{x} {y}'
+    return question
+
+
+def get_correct_answer(numbers: str) -> str:
+    x, y = numbers.split(' ')
+    x = int(x)
+    y = int(y)
+    gcd = math.gcd(x, y)
+    return str(gcd)
+
+
+def run():
+    game_explanation = 'Find the greatest common divisor of given numbers.'
+    game.run(game_explanation, get_question, get_correct_answer)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,9 @@
+import brain_games.games.gcd as gcd
+
+
+def main():
+    gcd.run()
+
+
+if __name__ == '__main__':
+    main()
